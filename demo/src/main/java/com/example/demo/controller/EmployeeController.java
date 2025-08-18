01918a95c61555d7c65233d1a8c4a4dd3c48fb07
purush <purush123.cs@gmail.com>
2025-08-18 20:40:33 +1000

    updated request mapping

--- demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -9,18 +9,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @PostMapping("/employees")
+    @PostMapping
     public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeCreateRequest employeeCreateRequest) {
         EmployeeDTO employee = employeeService.createEmployee(employeeCreateRequest);
         return ResponseEntity.status(HttpStatus.OK).body(employee);
     }
 
-    @GetMapping("/employees/{id}")
+    @GetMapping("/{id}")
     private ResponseEntity<EmployeeDTO> getEmployeeDetails(@PathVariable("id") int id) {
         EmployeeDTO employee = employeeService.getEmployeeById(id);
         return ResponseEntity.status(HttpStatus.OK).body(employee);
